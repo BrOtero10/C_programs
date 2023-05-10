fdff4c5516319f2cb52599e029ed54ee69850ab1
BrOtero10 <128537888+BrOtero10@users.noreply.github.com>
2023-05-09 22:08:15 -0300

    Create numero_primo.c
    Atividade de Algoritmo e Programação

---
+++ numero_primo.c
@@ -0,0 +1,36 @@
+/*
+numero_primo
+By: Bruno Otero - 1sem2023TADS - 09/05
+*/
+
+#include <stdio.h>
+
+int main(){
+    int number, i, conditional = 0;
+
+    printf("Digite um numero: ");
+    scanf("%d", &number);
+
+
+    //Excessões de numeros primos
+    if(number == 1 || number == 2){
+        printf("E' um numero primo.");
+    }
+
+    else{
+
+        for(i = number - 1; i > 2; i--){
+
+            //Testa se a divisão do número por qualquer antecessor seu se iguala a 0
+            if((number % i) == 0){
+                printf("Nao e' um numero primo.");
+                conditional++;
+                break;
+            }
+        }
+
+        //Só exibirá se o número for primo
+        if(conditional == 0)
+            printf("E' um numero primo.");
+    }
+}
