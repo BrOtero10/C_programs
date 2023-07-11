7b37ac171a4e92d374a54940310932c5da4a5810
Ana Paula Almeida Otero <bruno2004.aotero@gmail.com>
2023-07-10 22:29:54 -0300

    Finalização_reverter_ordem

--- desafio_pwc_codigos/reverter_ordem.c
+++ desafio_pwc_codigos/reverter_ordem.c
@@ -24,45 +24,76 @@ void separar_palavras(Word *palavras, char frase[500]) {
       int j = 0;
 
       if (frase[i] != ' ') {
-        while (frase[i] != ' ' && frase[i]) {
+        while (frase[i] != ' ' && frase[i] != '\n') {
           palavra[j] = frase[i];
           j++;
           i++;
         }
-
+        
         palavra[j] = '\0';
         strncpy(novo->palavra, palavra, 25);
         novo->proxima = malloc(sizeof(Word));
         novo = novo->proxima;
-        i++;
+        if(frase[i] != '\n')
+          i++;
       }
     }
   }
 }
 
-//procedimento usado nos testes durante a criaç]ao da lista de palavras
-
+//procedimento usado nos testes durante a criação da lista de palavras
+/*
 void imprimir_word(Word *word) {
   Word *auxiliar = word;
 
-  if (auxiliar) {
-    printf("\nPalavras:\n\n");
+  if(auxiliar) {
+    printf("\nPalavras:\n");
     while (auxiliar) {
-      printf("%s\n", auxiliar->palavra);
+      printf("\n%s", auxiliar->palavra);
       auxiliar = auxiliar->proxima;
     }
+    printf("\nFim Palavras.\n\n");
   }
 }
+*/
+//procedimento para imprimir na ordem reversa
+
+void imprimir_word_reverso(Word *word){
+  int tamanho = 0;
+  Word *auxiliar = word;
+
+  if(auxiliar){
+    printf("\n");
+    while(auxiliar -> proxima){
+    tamanho++;
+    auxiliar = auxiliar -> proxima;
+    }
+
+    for(int i = tamanho; i >= 0; i--){
+      auxiliar = word;
+  
+      for(int j = 0; j < i; j++)
+        auxiliar = auxiliar -> proxima;
+  
+      printf("%s ", auxiliar -> palavra);
+    }
+  }
+  
+
+}
+
+
 
 int main() {
   char frase[500]; // frase com no máximo 500 caracteres
   fgets(frase, 500, stdin);
 
   Word *palavras = malloc(sizeof(Word));
+  memset(palavras, 0, sizeof(Word)); //Remove lixo de memoria
 
   separar_palavras(palavras, frase);
-
-  imprimir_word(palavras);
+  
+  imprimir_word_reverso(palavras);
 
   return 0;
 }
